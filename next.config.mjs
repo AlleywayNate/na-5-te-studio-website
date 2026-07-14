fd8e3cc5ed6c502a56e72f6b43396736a328ace0
Nate <72818693+AlleywayNate@users.noreply.github.com>
2026-07-13 20:51:16 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Tells Next.js to generate a static HTML/CSS/JS export
+  basePath: '/na-5-te-studio-website', // Matches your GitHub repository name
   typescript: {
     ignoreBuildErrors: true,
   },
